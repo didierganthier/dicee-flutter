9b1947da084bc1530b29cbabf2b731219ae4de47
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-04-16 13:29:20 -0400

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +18,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget{
+  @override
+  DicePageState createState() => DicePageState();
+
+}
+
+class DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  generateRandomNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                    onPressed: () {
+                     generateRandomNumber();
+                    },
+                  )
+              ),
+              Expanded(
+                  child: FlatButton(
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                    onPressed: () {
+                      generateRandomNumber();
+                    },
+                  )
+              )
+            ]
+        ),
+    );
   }
 }
